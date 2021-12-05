e7d54daa21600532111c040d20eed87897d454de
Nguyễn Duy Hưng <duyhungnguyen2002@gmail.com>
2021-12-05 11:12:14 +0700

    Commit 11h15 5/12/2021 - Nội dung đã làm: + Sửa lại class GameLauncher

--- src/main/java/GameLauncher.java
+++ src/main/java/GameLauncher.java
@@ -1,65 +1,11 @@
 package main.java;
 
-import javafx.animation.AnimationTimer;
-import javafx.application.Application;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.stage.Stage;
-import main.java.Entities.Entity;
-import main.java.Graphics.Sprite;
-import java.util.ArrayList;
-import java.util.List;
+import main.java.GUI.Frame;
+import main.java.Sound.Sound;
 
-public class GameLauncher extends Application {
-
-    public static final int WIDTH = 31;
-    public static final int HEIGHT = 13;
-
-    private GraphicsContext gc;
-    private Canvas canvas;
-    private List<Entity> entities = new ArrayList<>();
-    private List<Entity> stillObjects = new ArrayList<>();
-    private List<Entity> itemsList = new ArrayList<>();
-    private List<String> map = new ArrayList<>();
-
+public class GameLauncher {
     public static void main(String[] args) {
-        Application.launch(GameLauncher.class);
-    }
-
-    @Override
-    public void start(Stage stage) {
-        // Tao Canvas
-        canvas = new Canvas(Sprite.SCALED_SIZE * WIDTH, Sprite.SCALED_SIZE * HEIGHT);
-        gc = canvas.getGraphicsContext2D();
-
-        // Tao root container
-        Group root = new Group();
-        root.getChildren().add(canvas);
-
-        // Tao scene
-        Scene scene = new Scene(root);
-
-        // Them scene vao stage
-        stage.setScene(scene);
-        stage.show();
-        AnimationTimer timer = new AnimationTimer() {
-            @Override
-            public void handle(long l) {
-                render();
-                update();
-            }
-        };
-        timer.start();
-
-    }
-
-    public void update() {
-        entities.forEach(Entity::update);
-    }
-
-    public void render() {
-        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        Sound.play("soundtrack");
+        new Frame();
     }
 }
