ea9a76d1ee2ff5afbd6bee8462eaac9fcbb5fd19
HoangLam472 <83564729+HoangLam472@users.noreply.github.com>
2021-12-05 09:01:40 +0700

    Commit 05/12/2021 Nội dung: -Sửa lớp Sound.

--- src/main/java/Sound/Sound.java
+++ src/main/java/Sound/Sound.java
@@ -1,41 +1,21 @@
 package main.java.Sound;
 
-import sun.applet.Main;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.AudioInputStream;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import java.io.File;
 
 
 public class Sound {
     public static void play(String sound) {
-        new Thread(new Runnable() {
-            public void run() {
-                try {
-                    Clip clip = AudioSystem.getClip();
-                    AudioInputStream inputStream = AudioSystem.getAudioInputStream(
-                            Main.class.getResourceAsStream("/sound/" + sound + ".mp3"));
-                    clip.open(inputStream);
-                    clip.start();
-                } catch (Exception e) {
-                    System.err.println(e.getMessage());
-                }
-            }
-        }).start();
-
+        String mp3 = sound + ".mp3";
+        Media media = new Media(new File(mp3).toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.play();
     }
     public static void stop(String sound){
-        new Thread(new Runnable() {
-            public void run() {
-                try {
-                    Clip clip = AudioSystem.getClip();
-                    AudioInputStream inputStream = AudioSystem.getAudioInputStream(
-                            Main.class.getResourceAsStream("/sound/" + sound + ".mp3"));
-                    clip.open(inputStream);
-                    clip.stop();
-                } catch (Exception e) {
-                    System.err.println(e.getMessage());
-                }
-            }
-        }).start();
+        String mp3 = sound + ".mp3";
+        Media media = new Media(new File(mp3).toURI().toString());
+        MediaPlayer mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.pause();
     }
 }
